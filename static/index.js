ab72a5c4d0ef99520f9eb04da1f797d8486a393f
ind-spirit <error156@yandex.ru>
2021-08-19 22:09:56 +0700

    photos page address bar fix

--- static/index.js
+++ static/index.js
@@ -1,19 +1,6 @@
 window.onload = () => {
     const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
-    console.log(ifMobile);
-    console.log(document.body.style.height, window.innerHeight);
     if (ifMobile) {
-        // document.body.style.height = window.innerHeight;
         document.body.style.height = `${window.innerHeight}px`;
-        
     }
-    console.log(document.body.style.height, window.innerHeight);
-    console.log(document.body.style, document.body.offsetHeight);
-    console.log(document.body.offsetHeight);
-
-
-
-
-
-
 }
--- static/pages/photos/photos.js
+++ static/pages/photos/photos.js
@@ -137,6 +137,13 @@
 
 
         //calling functions
+        
+        //MOBILE ADDRESS BAR FIX
+        const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
+        if (ifMobile) {
+            document.body.style.height = `${window.innerHeight}px`;
+        }
+
         adaptImagesWidth();
         createPlaceholders();
         lazyLoading();
