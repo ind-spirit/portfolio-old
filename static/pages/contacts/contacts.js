5e616ce6771de6a90f5712d139334a5f82940a5b
ind-spirit <error156@yandex.ru>
2021-08-26 22:19:46 +0700

    lock screen orientation

--- static/pages/contacts/contacts.js
+++ static/pages/contacts/contacts.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    screen.orientation.lock('portrait');
     const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
     if (ifMobile) {
         document.body.style.height = `${window.innerHeight}px`;
