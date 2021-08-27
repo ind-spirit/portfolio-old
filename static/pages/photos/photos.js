e9ec1adead1da4788562ceca116a9664401eae75
ind-spirit <error156@yandex.ru>
2021-08-27 15:59:21 +0700

    searchbar fix

--- static/pages/photos/photos.js
+++ static/pages/photos/photos.js
@@ -139,12 +139,12 @@
         //calling functions
 
         //MOBILE ADDRESS BAR FIX
-        const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
-        if (ifMobile) {
-            document.body.style.height = `${window.innerHeight}px`;
-            back_wrapper.style.height = `${window.innerHeight}px`;
-            gallery.style.height = `${window.innerHeight}px`;
-        }
+        // const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
+        // if (ifMobile) {
+        //     document.body.style.height = `${window.innerHeight}px`;
+        //     back_wrapper.style.height = `${window.innerHeight}px`;
+        //     gallery.style.height = `${window.innerHeight}px`;
+        // }
 
         adaptImagesWidth();
         createPlaceholders();
